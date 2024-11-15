b4c25023b24d4e3feb3fe08311260c0e6bfbee54
constt <mattheo.hering@gmail.com>
2024-11-15 08:52:52 +0100

    added a index to every formupdate variable

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import { enhance } from "$app/forms";
     export let data;
-    // TODO: Make form update a array. give each block item a index. at 16 and 19 bind this to formupdate array with the index number
     let form_update: HTMLFormElement[];
+    function submitForm(index: number) {
+        if(form_update[index]) {
+            form_update[index].submit();
+            console.log(form_update)
+        }
+    }
 </script>
 
 <form action="/dashboard?/createTodo" method="POST" use:enhance>
@@ -10,13 +15,13 @@
     <button>Send</button>
 </form>
 
-{#each data.todos as todo}
+{#each data.todos as todo, index}
     <div class="flex">
         <p>{todo.todo}</p>
-        <form action="/dashboard?/update" bind:this={form_update}>
+        <form action="/dashboard?/update" bind:this={form_update[index]}>
             <input type="hidden" value="{todo.id}" name="id">
             <input type="checkbox" checked={todo.status} name="checked" on:change={()=> {
-                form_update.submit();
+                submitForm(index);
             }}>
         </form>
     </div> 
